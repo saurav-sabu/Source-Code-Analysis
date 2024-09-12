94a42a1ce693fb136343189c735a97929255c67e
Saurav Sabu <saurav.sabu9@gmail.com>
2024-09-12 19:53:34 +0530

    setup.py and requirements.txt updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Source Code Analysis",
+    version="0.0.0",
+    author="Saurav Sabu",
+    author_email="saurav.sabu9@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
